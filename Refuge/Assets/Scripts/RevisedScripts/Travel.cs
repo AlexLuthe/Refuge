a0b08889a1f96b8e3b366ccbfe4a80c92d44d843
J-Dodds <1004254@student.sae.edu.au>
2017-11-02 19:25:02 +1000

    Fixed some merge errors I had

--- Refuge/Assets/Scripts/RevisedScripts/Travel.cs
+++ Refuge/Assets/Scripts/RevisedScripts/Travel.cs
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class Travel : MonoBehaviour {
-
-    public GameObject backGround;
-    public GameObject[] backGroundObjs;
-    public GameObject[] foreGroundObjs;
-    public GameObject[] characters;
-    public float partySpeed = 1;
-    public float timer = 200;
-    public int hubToSwitch;
-    GameManager_r GM;
-    
-	// Use this for initialization
-	void Start () {
-        GM = GameObject.Find("GameManager").GetComponent<GameManager_r>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        backGround.transform.Translate(new Vector3(0.15f * partySpeed, 0));
-        foreach (GameObject backGround in backGroundObjs) {
-        backGround.transform.Translate(new Vector3(0.5f * partySpeed, 0));
-        if (backGround.transform.position.x > Screen.width + backGround.GetComponent<RectTransform>().rect.width / 4)
-            backGround.transform.position = new Vector3(0 - backGround.GetComponent<RectTransform>().rect.width / 4, backGround.transform.position.y);
-        }
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -60,19 +29,11 @@ public class Travel : MonoBehaviour {
         if (backGround.transform.position.x > Screen.width + backGround.GetComponent<RectTransform>().rect.width / 4)
             backGround.transform.position = new Vector3(0 - backGround.GetComponent<RectTransform>().rect.width / 4, backGround.transform.position.y);
         }
->>>>>>> a2453b4301955750bb1b385d8ba6449ed0bdd1b7
+
         foreach (GameObject foreGround in foreGroundObjs) {
             foreGround.transform.Translate(new Vector3(1 * partySpeed, 0));
             if (foreGround.transform.position.x > Screen.width + foreGround.GetComponent<RectTransform>().rect.width / 4)
                 foreGround.transform.position = new Vector3(0 - foreGround.GetComponent<RectTransform>().rect.width / 4, foreGround.transform.position.y);
-<<<<<<< HEAD
-        }
-        if (timer <= 0)
-            GM.SwitchToHub(hubToSwitch);
-        timer -= Time.deltaTime;
-	}
-
-=======
         }
         if (timer <= 0)
             if (camped)
@@ -84,7 +45,6 @@ public class Travel : MonoBehaviour {
         timer -= Time.deltaTime;
 	}
 
->>>>>>> a2453b4301955750bb1b385d8ba6449ed0bdd1b7
     private void OnEnable() {
         for (int i = 0; i < GM.characters.Length; ++i) {
             if (GM.characters[i].GetComponent<Character_r>().isDead)
